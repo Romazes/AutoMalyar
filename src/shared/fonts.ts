dc9dafe255a88d96f546cf4ca14a307a4ae9906c
Romazes <romazes20@gmail.com>
2023-07-19 22:52:02 +0300

    feat: setup eslint - add app' fonts - add upper to tailwind config

---
+++ src/shared/fonts.ts
@@ -0,0 +1,14 @@
+import localFont from "next/font/local";
+
+export const fontDameron = localFont({
+  src: "../../public/fonts/dameron.ttf",
+  variable: "--font-dameron",
+});
+
+export const fontJost = localFont({
+  src: [
+    { path: "../../public/fonts/Jost-Medium.ttf", weight: "500" },
+    { path: "../../public/fonts/Jost-Regular.ttf", weight: "400" },
+  ],
+  variable: "--font-jost",
+});
