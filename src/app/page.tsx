21060bbf995ec08ae32cb37981d69586f3359890
Romazes <romazes20@gmail.com>
2023-07-23 23:51:04 +0300

    feat: impl NavBar - hamburger icon - main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
+import Image from 'next/image';
+
+import FerrariBgImage from '@/../public/images/ferrari-bg.png';
 import { Button } from '@/components/Buttons';
 import { GradientArrowIcon } from '@/components/icons/GradientArrow';
 
 const Home = () => {
   return (
-    <main className="pl-36 text-white">
-      <div className="flex">
+    <main className="pl-9 text-white lg:pl-36">
+      <div className="font-dameron">
+        <h1 className="text-4xl leading-default-normal md:text-6-5xl">
+          Auto
+          <br />
+          malyar
+        </h1>
+      </div>
+      <div className="flex flex-col-reverse md:flex-row">
         <div>
-          <div>
-            <h1 className="font-dameron text-6-5xl leading-default-normal">
-              Auto
-              <br />
-              malyar
-            </h1>
-          </div>
-          <div></div>
           <div className="mt-20">
             <p className="text font-jost">
               Професіональне фарбування авто,
@@ -47,8 +49,17 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div>
-          <GradientArrowIcon />
+        <div className="relative grow">
+          <div className="mt-[-67.2px] md:mt-[-118.4px] clear-both overflow-auto w-[432.52px] h-[320px]">
+            <GradientArrowIcon />
+          </div>
+          <div>
+            <Image
+              src={FerrariBgImage}
+              alt="background image"
+              className="absolute bottom-[-40px] right-0"
+            />
+          </div>
         </div>
       </div>
     </main>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,33 @@
+import Link from 'next/link';
 import React from 'react';
+
+import { HamburgerIcon } from '../icons/Hamburger';
 
 export const Navbar = () => {
   return (
-    <nav className="flex w-full items-center justify-between">
-      <div>one</div>
-      <div>two</div>
+    <nav className="ml-9 mr-9 flex h-[92px] items-center justify-between font-jost uppercase text-white/30 md:ml-12 md:mr-12 lg:ml-20 lg:mr-16">
+      <div>LOGO</div>
+      <div className="cursor-pointer md:hidden">
+        <HamburgerIcon />
+      </div>
+      <div className="hidden md:block">
+        <ul className="flex gap-16">
+          <li>
+            <Link href="#" className="text-white">
+              Головна
+            </Link>
+          </li>
+          <li>
+            <Link href="#">Сервіси</Link>
+          </li>
+          <li>
+            <Link href="#">Контакти</Link>
+          </li>
+          <li>
+            <Link href="#">галерея</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
