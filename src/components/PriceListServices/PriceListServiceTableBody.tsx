4bf1852d533f9fdbca2f04677fd6d52fc1df510a
Romazes <romazes20@gmail.com>
2023-08-21 18:15:36 +0300

    fix: prettier rules

--- src/components/PriceListServices/PriceListServiceTableBody.tsx
+++ src/components/PriceListServices/PriceListServiceTableBody.tsx
@@ -6,7 +6,9 @@ interface IPriceListServiceTableBody {
   duration: string;
 }
 
-export const PriceListServiceTableBody: FC<IPriceListServiceTableBody> = (props) => {
+export const PriceListServiceTableBody: FC<IPriceListServiceTableBody> = (
+  props,
+) => {
   return (
     <div className="grid grid-cols-2 grid-rows-[37px_52px] items-center text-center text-white/50 lg:grid-cols-3 lg:grid-rows-[63px] lg:hover:bg-[#FFFFFF]/[.05]">
       <div className="col-span-2 flex h-full items-center justify-center bg-[#FFFFFF]/[.03] lg:col-span-1 lg:bg-inherit">
