19a6d58f37c34567cd78a6efa84bb14a146c7098
Romazes <romazes20@gmail.com>
2023-08-23 19:11:19 +0300

    feat: begin responsive navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,20 +1,23 @@
+'use client';
+import clsx from 'clsx';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { HamburgerIcon } from '../icons/Hamburger';
 import { LogoAutoMalyar } from '../icons/Logo';
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="top-0 z-10 flex h-[92px] items-center justify-between bg-primary pl-9 pr-9 font-jost uppercase text-white/30 md:pl-12 md:pr-12 lg:pl-20 lg:pr-16">
-      <div>
-        <LogoAutoMalyar
-          className="visible md:invisible"
-          width={236}
-          height={86}
-        />
+      <div className="z-4">
+        <LogoAutoMalyar width={127} height={32} />
       </div>
-      <div className="cursor-pointer md:hidden">
+      <div
+        className="z-4 cursor-pointer md:hidden"
+        onClick={() => setIsOpen(!isOpen)}
+      >
         <HamburgerIcon />
       </div>
       <div className="hidden md:block">
@@ -33,6 +36,12 @@ export const Navbar = () => {
           </li>
         </ul>
       </div>
+      <div
+        className={clsx(
+          'fixed z-3 h-8 w-8 rounded-[50%] bg-primary transition-transform duration-[1000ms] top-[30px] right-[40px]',
+          isOpen ? 'scale-[90]' : 'scale-0',
+        )}
+      ></div>
     </nav>
   );
 };
