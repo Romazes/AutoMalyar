2c7fc8b5ec025748dd53a62ed712917a31f7f216
Romazes <romazes20@gmail.com>
2023-08-25 15:59:55 +0300

    refactor: finished navbar for mobile

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,47 +1,78 @@
 'use client';
 import clsx from 'clsx';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { HamburgerIcon } from '../icons/Hamburger';
 import { LogoAutoMalyar } from '../icons/Logo';
+import { InstagramIcon } from '../icons/SocialNetworks/Instagram';
+import { TikTokIcon } from '../icons/SocialNetworks/TikTok';
+
+import { SOCIAL_NETWORK_LINKS } from '@/constants/contacts';
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const year = useMemo(() => new Date().getFullYear(), []);
 
   return (
-    <nav className="top-0 z-10 flex h-[92px] items-center justify-between bg-primary pl-9 pr-9 font-jost uppercase text-white/30 md:pl-12 md:pr-12 lg:pl-20 lg:pr-16">
-      <div className="z-4">
+    <nav
+      className={clsx(
+        'grid grid-cols-2 items-center bg-primary px-9 font-jost uppercase text-white/30 md:grid-cols-none md:px-12 lg:pl-20 lg:pr-16',
+        isOpen
+          ? 'grid-rows-[auto_1fr_1fr]'
+          : 'md:grid-flow-col md:justify-between',
+      )}
+    >
+      <div className="z-4 my-9">
         <LogoAutoMalyar width={127} height={32} />
       </div>
       <div
-        className="z-4 cursor-pointer md:hidden"
+        className="z-4 my-9 cursor-pointer justify-self-end md:hidden"
         onClick={() => setIsOpen(!isOpen)}
       >
         <HamburgerIcon />
       </div>
-      <div className="hidden md:block">
-        <ul className="flex gap-16">
-          <li className="hover:text-white">
-            <Link href="#main-section">Головна</Link>
-          </li>
-          <li className="hover:text-white">
-            <Link href="#painting-section">Сервіси</Link>
-          </li>
-          <li className="hover:text-white">
-            <Link href="#info-contacts-section">Контакти</Link>
-          </li>
-          <li className="hover:text-white">
-            <Link href="#gallery-section">галерея</Link>
-          </li>
-        </ul>
+      <div
+        className={clsx('md:block', isOpen ? 'z-4 col-span-2 mt-16' : 'hidden')}
+      >
+        <div className="text-center md:block">
+          <ul className="flex flex-col gap-16 md:flex-row">
+            <li className="hover:text-white">
+              <Link href="#main-section">Головна</Link>
+            </li>
+            <li className="hover:text-white">
+              <Link href="#painting-section">Сервіси</Link>
+            </li>
+            <li className="hover:text-white">
+              <Link href="#info-contacts-section">Контакти</Link>
+            </li>
+            <li className="hover:text-white">
+              <Link href="#gallery-section">галерея</Link>
+            </li>
+          </ul>
+        </div>
       </div>
       <div
         className={clsx(
-          'fixed z-3 h-8 w-8 rounded-[50%] bg-primary transition-transform duration-[1000ms] top-[30px] right-[40px]',
-          isOpen ? 'scale-[90]' : 'scale-0',
+          'md:hidden',
+          isOpen ? 'z-4 col-span-2 grid gap-y-10' : 'hidden',
         )}
-      ></div>
+      >
+        <div>
+          <div className="flex">
+            <div className="flex gap-x-5">
+              <div>Більше проектів тут:</div>
+              <a href={SOCIAL_NETWORK_LINKS.instagram} target="_blank">
+                <InstagramIcon />
+              </a>
+              <a href={SOCIAL_NETWORK_LINKS.tiktok} target="_blank">
+                <TikTokIcon />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="">© {year} «Automalyar». Всі права захищені.</div>
+      </div>
     </nav>
   );
 };
