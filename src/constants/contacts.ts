b43c2ccd9a3958fc339648e5a430187a678b9432
Romazes <romazes20@gmail.com>
2023-09-06 15:17:18 +0300

    fix: phone numbers

--- src/constants/contacts.ts
+++ src/constants/contacts.ts
@@ -1,10 +1,10 @@
 export const PHONE_MAIN = {
-  machineView: '+380661234567',
-  humanView: '+38 (066) 123-45-67',
+  machineView: '+380993168890',
+  humanView: '+38 (099) 316-88-90',
 };
 export const PHONE_SECOND = {
-  machineView: '+380678910121',
-  humanView: '+38 (067) 891-01-21',
+  machineView: '+380978359141',
+  humanView: '+38 (097) 835-91-41',
 };
 export const MAILING_ADDRESS = 'automalyar@service.ua';
 export const STREET_ADDRESS = {
